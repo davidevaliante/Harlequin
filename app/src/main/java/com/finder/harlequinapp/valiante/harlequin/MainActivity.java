02c2cf1c17d90ad0f272947d1d96432dbb7e681d
akain <akainurev@gmail.com>
2016-11-05 15:13:38 +0100

    Prova commit iniziale

--- app/src/main/java/com/finder/harlequinapp/valiante/harlequin/MainActivity.java
+++ app/src/main/java/com/finder/harlequinapp/valiante/harlequin/MainActivity.java
@@ -35,12 +35,11 @@ public class MainActivity extends Activity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-    //prova master branch update
+
     //TODO devo aggiungere il lock nella UserPage per evitare che l'utente torni in questa pagina
     //TODO il lock dopo aver fatto il login adesso funziona ma solo se si spamma il pulsante indietro, va migliorato
 
-    //Stringa di prova per branch
-    //Stringa di prova per branch con il secondo commit
+    //prova commit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
